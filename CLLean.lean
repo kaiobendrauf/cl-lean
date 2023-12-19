5a6fb2938e6dc85fff1ebfb692f47162a9b80c6f
Vierkantor <vierkantor@vierkantor.com>
2023-12-19 14:58:19 +0000

    Move to a Lean 4 directory structure (very much broken still!)

---
+++ CLLean.lean
@@ -0,0 +1,28 @@
+-- This module serves as the root of the `CLLean` library.
+-- Import modules here that should be built as part of the library.
+
+import CLLean.completeness.canonical_filtering
+import CLLean.completeness.canonical
+import CLLean.completeness.closureCLC
+import CLLean.completeness.closureCLK
+import CLLean.completeness.completenessCLC
+import CLLean.completeness.completenessCLK
+import CLLean.completeness.completenessCL
+import CLLean.completeness.completenessCLtrueplay
+import CLLean.semantics.model
+import CLLean.semantics.playability
+import CLLean.semantics.semanticsCLC
+import CLLean.semantics.semanticsCLK
+import CLLean.semantics.semanticsCL
+import CLLean.soundness.soundnessCLC
+import CLLean.soundness.soundnessCLK
+import CLLean.soundness.soundnessCL
+import CLLean.syntax.CLCLemmas
+import CLLean.syntax.CLKLemmas
+import CLLean.syntax.CLLemmas
+import CLLean.syntax.consistency
+import CLLean.syntax.formula
+import CLLean.syntax.propLemmas
+import CLLean.syntax.syntaxCLC
+import CLLean.syntax.syntaxCLK
+import CLLean.syntax.syntaxCL
--- scripts/cleanup-braces.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -exo pipefail
-
-find src -name '*.lean' | xargs ./scripts/cleanup-braces.py
